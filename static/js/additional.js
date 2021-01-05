33792a1608b7ddbc2d1f65361865bc64b4748340
Daria Maximova <daria.maximova.m@gmail.com>
2021-01-05 19:08:34 +0300

    actual versions of materialize comps + CSS for sticky footer, JS for selects

---
+++ static/js/additional.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('select');
+   var options = document.querySelectorAll('option');
+   var instances = M.FormSelect.init(elems, options); })
