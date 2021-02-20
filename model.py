f193a66b5ab47e3ecd78549f8294986f83db20b8
Daria Maximova <daria.maximova.m@gmail.com>
2021-02-20 10:17:18 +0300

    switch to ORM instead of direct SQL requests

--- model.py
+++ model.py
@@ -6,27 +6,23 @@ from sqlalchemy.orm import relationship
 db = SQLAlchemy()
 
 
-class Photo(db.Model):
-    __tablename__ = "PhotoDescriptions"
-
-    id_photo = db.Column("idPhoto", db.Integer, primary_key=True)
-    year = db.Column("year", db.Integer)
-    photo_description = db.Column("photoDescription", db.Text)
-    # location
-    id_location = db.Column("idLocation", db.Integer, ForeignKey("Locations.idLocation"))
-    location = db.relationship("Location")
-    # author
-    id_author = db.Column("idAuthor", db.Integer, ForeignKey("Authors.idAuthor"))
-    author = db.relationship("Author")
-    # rubrics
-    rubrics = db.relationship("Rubric", secondary="PhotoRubrics", lazy="dynamic")
-
-
 class Author(db.Model):
     __tablename__ = "Authors"
 
     id_author = db.Column("idAuthor", db.Integer, primary_key=True)
     author_name = db.Column("authorName", db.Text)
+
+
+class MuseumDescription(db.Model):
+    __tablename__ = "MuseumAnnotations"
+    __table_args__ = (PrimaryKeyConstraint("idPhoto"),)
+
+    id_photo = db.Column("idPhoto", db.Integer, ForeignKey("PhotoDescriptions.idPhoto"))
+    id_identif = db.Column("idIdentif", db.Text)
+    id_inv = db.Column("idInv", db.Text)
+    original_date = db.Column("originalDate", db.Text)
+    original_author = db.Column("originalAuthor", db.Text)
+    original_location = db.Column("originalLocation", db.Text)
 
 
 class Location(db.Model):
@@ -57,5 +53,29 @@ class Files(db.Model):
     __table_args__ = (PrimaryKeyConstraint("idPhoto"),)
 
     id_photo = db.Column("idPhoto", db.Integer, ForeignKey("PhotoDescriptions.idPhoto"))
-    fullsize_file = db.Column("fullPath", db.Text)
-    miniature_file = db.Column("miniPath", db.Text)
+    fullsize_file = db.Column("fullsizeFile", db.Text)
+    miniature_file = db.Column("miniatureFile", db.Text)
+
+
+class Photo(db.Model):
+    __tablename__ = "PhotoDescriptions"
+
+    id_photo = db.Column("idPhoto", db.Integer, primary_key=True)
+    title = db.Column("photoName", db.Text)
+
+    year = db.Column("year", db.Integer)
+    photo_description = db.Column("photoDescription", db.Text)
+
+    museum_comments = db.relationship("MuseumDescription")
+    files = db.relationship("Files")
+
+    # location
+    id_location = db.Column("idLocation", db.Integer, ForeignKey("Locations.idLocation"))
+    location = db.relationship("Location")
+
+    # author
+    id_author = db.Column("idAuthor", db.Integer, ForeignKey("Authors.idAuthor"))
+    author = db.relationship("Author")
+
+    # rubrics
+    rubrics = db.relationship("Rubric", secondary="PhotoRubrics", lazy="dynamic")
